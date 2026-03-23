0a2313ceb7d1505c91ba8a0a4c0f37c2cbf19f1a
Piyush Sharma <sharmapiyush1342@gmail.com>
2026-03-23 14:11:41 +0530

    day work

--- header.php
+++ header.php
@@ -4,6 +4,6 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="canonical" href="<?php echo get_permalink(); ?>" />
-    <?php wp_head(); ?>
+<link rel="preload" as="image" href="<?php echo get_template_directory_uri(); ?>/img/background-web-1.webp">    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
