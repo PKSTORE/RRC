b4bf6e2903eec7fb42f428fb175da5e7082c3baf
Piyush Sharma <sharmapiyush1342@gmail.com>
2026-03-23 17:25:07 +0530

    update the theme

--- page-rental.php
+++ page-rental.php
@@ -86,7 +86,7 @@
         </div>
       </div>
       <div class="card">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/M1-logo.png" alt="Dell-5400">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/M1-1.png" alt="Dell-5400">
         <div class="card-body">
         <h3>Apple MacBook Air M1</h3>
           <p class="specs"><span class="Processor">Processor:</span> Apple M1 chip with 8-core CPU and 7/8-core GPU
