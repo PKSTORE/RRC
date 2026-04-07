94ccd53c25e500b481a75cdd50829f22aad42cde
Piyush Sharma <sharmapiyush1342@gmail.com>
2026-04-07 12:21:33 +0530

    day work

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="canonical" href="<?php echo get_permalink(); ?>" />
+    <link rel="canonical" href="<?php echo esc_url( home_url( add_query_arg( [], $_SERVER['REQUEST_URI'] ) ) ); ?>" />
 <link rel="preload" as="image" href="<?php echo get_template_directory_uri(); ?>/img/background-web-1.webp">    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
